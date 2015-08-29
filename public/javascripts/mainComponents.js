a289004351311da3cf7745cf92072937f4b7c919
Kimokeo Rabang <kimokerava@gmail.com>
2015-08-29 12:54:29 -0400

    added header and front end changes

--- public/javascripts/mainComponents.js
+++ public/javascripts/mainComponents.js
@@ -28,7 +28,7 @@ var MainBox = React.createClass({
 				</div>
 				<BlueBand />
 				<h3>Tell Responder</h3>
-				<Address />
+				<Address url="reverseGeocode"/>
 				<CallButton />
 				<GeoLocate />
 			</div>
@@ -37,6 +37,32 @@ var MainBox = React.createClass({
 });
 
 var Address = React.createClass({
+	loadReverseGeolocationFromServer: function () {
+		console.log(this);
+		navigator.geolocation.getCurrentPosition(function (position) {
+		    $.ajax({
+		        url: this.props.url,
+		        dataType: 'json',
+		        method: 'GET',
+		        data: {
+		            'latitude': position.coords.latitude,
+		            'longitude': position.coords.longitude
+		        },
+		        success: function (data) {
+		        	this.setState({data: data});
+		        }.bind(this),
+		        error: function (x,s,e) {
+		            console.error(s,e);
+		        }.bind(this)
+		    });
+		}.bind(this));
+	},
+	getInitialState: function () {
+		return {data: {}};
+	},
+	componentDidMount: function () {
+		this.loadReverseGeolocationFromServer();
+	},
 	render: function(){
 		return (
 		<div className="addressWrapper">
@@ -44,7 +70,7 @@ var Address = React.createClass({
 			<img src="../images/speechArrow.svg"></img>
 		</div>
 		<div className="locationAddress">
-			<h3>I am at 233 VARIABLE st</h3>
+			<h3>{this.state.data.location}</h3>
 		</div>
 		</div>
 		);
